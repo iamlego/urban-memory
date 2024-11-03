77ccb4b89dbdaea5d45c78785a95a7b37ef2e04a
iamlego <sairaj.jadhav812@gmail.com>
2024-11-02 18:33:53 -0700

    sentiment analysis route

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -2,26 +2,26 @@ const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
 
-// Search for gifts
+
 router.get('/', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
+        
         const db = await connectToDatabase();
 
         const collection = db.collection("secondChanceItems");
 
-        // Initialize the query object
+        
         let query = {};
 
-        // Add the name filter to the query if the name parameter is not empty
+        
         
         if(req.query.name && req.query.name.trim() !== ''){
-            query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
+            query.name = { $regex: req.query.name, $options: "i" }; 
         }
             
-        // }
+    
 
-        // Task 3: Add other filters to the query
+        
         if (req.query.category) {
             query.category = req.query.category;
         }
@@ -32,7 +32,7 @@ router.get('/', async (req, res, next) => {
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
+        
         const items = await collection.find(query).toArray()
 
         res.json(items);

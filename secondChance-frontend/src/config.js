af0cef90bdaf1abc1468fbdde62f9d7760da6619
iamlego <sairaj.jadhav812@gmail.com>
2024-11-04 22:24:48 -0800

    Integrated front-end and backend

--- secondChance-frontend/src/config.js
+++ secondChance-frontend/src/config.js
@@ -1,6 +1,11 @@
+// require('dotenv').config();
 const config = {
   backendUrl: process.env.REACT_APP_BACKEND_URL,
 };
 
 console.log(`backendUrl in config.js: ${config.backendUrl}`)
-export {config as urlConfig}
+// export {config as urlConfig}
+module.exports = { urlConfig: config };
+
+
+// console.log(process.env.REACT_APP_BACKEND_URL);
